43ec5787d7f998e1d4574d98928b58cbe2acdea9
Shivam Gupta <118615006+Shivamganesh@users.noreply.github.com>
2025-07-13 02:23:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import jobAnalyzer from "./routes/jobAnalyzer.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "2mb" }));
 
 app.use("/api/resume-analyzer", resumeAnalyzer);
 app.use("/api/job-analyzer", jobAnalyzer);
